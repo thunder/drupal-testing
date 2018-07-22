edcd3de2675a8b28c42bfc5b08d0d2f25f37e556
Daniel Bosen <daniel.bosen@burda.com>
2018-07-22 14:38:25 +0200

    Simplify and generalize (#1)
    * Move logic into functions
    
    * Use more generic default installation folder
    
    * Generalize bin directory

--- lib/testing-library.sh
+++ lib/testing-library.sh
@@ -42,36 +42,59 @@ require_local_project() {
     composer require ${THUNDER_TRAVIS_COMPOSER_NAME} --no-update --working-dir=${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}
 }
 
-install_with_composer() {
+composer_install() {
     composer install --optimize-autoloader --apcu-autoloader --working-dir=${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}
 }
 
 create_drupal_project() {
     composer create-project drupal-composer/drupal-project:8.x-dev ${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY} --stability dev --no-interaction --no-install
-
     composer require drupal/core:${THUNDER_TRAVIS_DRUPAL_VERSION} --no-update --working-dir=${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}
-    composer require webflo/drupal-core-require-dev:${THUNDER_TRAVIS_DRUPAL_VERSION} --dev --no-update --working-dir=${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}
-
-    require_local_project
-    install_with_composer
 }
 
 create_thunder_project() {
     composer create-project burdamagazinorg/thunder-project ${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY} --stability dev --no-interaction --no-install
-
     composer require burdamagazinorg/thunder:${THUNDER_TRAVIS_THUNDER_VERSION} --no-update --working-dir=${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}
+}
+
+create_project() {
+    local distribution=${1-"drupal"}
+
+    case ${distribution} in
+        "drupal")
+            create_drupal_project
+        ;;
+        "thunder")
+            create_thunder_project
+        ;;
+    esac
+
+    # The folder where composer puts binaries, the default value is read from the projects composer.json
+    # TODO: I do not like this here, but we cannot know what the projects bin directory is in the environment.sh
+    THUNDER_TRAVIS_COMPOSER_BIN_DIR=${THUNDER_TRAVIS_COMPOSER_BIN_DIR:-`jq -er '.config."bin-dir" // "vendor/bin"' ${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}/composer.json`}
+
     composer require webflo/drupal-core-require-dev:${THUNDER_TRAVIS_DRUPAL_VERSION} --dev --no-update --working-dir=${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}
 
     require_local_project
-    install_with_composer
+    composer_install
 }
 
-install_drupal() {
+install_project() {
+    local distribution=${1-"drupal"}
     local profile=${1-"standard"}
+
+    case ${distribution} in
+        "drupal")
+            profile="standard"
+        ;;
+        "thunder")
+            profile="thunder"
+        ;;
+    esac
+
     cd ${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}/$(distribution_docroot)
 
     php core/scripts/drupal install ${profile} --no-interaction
-    ${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}/vendor/bin/drush en simpletest
+    ${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}/${THUNDER_TRAVIS_COMPOSER_BIN_DIR}/drush en simpletest
 
     cd ${THUNDER_TRAVIS_PROJECT_BASEDIR}
 }
@@ -99,5 +122,5 @@ run_tests() {
     chmod u+w ${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}/${docroot}/sites/default/settings.php
     rm ${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}/${docroot}/sites/default/settings.php
 
-    php ${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}/vendor/bin/phpunit --verbose -c ${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}/${docroot}/core ${test_selection}
+    php ${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}/${THUNDER_TRAVIS_COMPOSER_BIN_DIR}/phpunit --verbose -c ${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}/${docroot}/core ${test_selection}
 }
