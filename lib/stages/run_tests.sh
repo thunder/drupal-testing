d0b6064f7669d93679ea29c87e206fde29f9c88c
Christian Fritsch <chr.fritsch@gmx.net>
2020-02-04 17:22:12 +0100

    feat: add paratest support

--- lib/stages/run_tests.sh
+++ lib/stages/run_tests.sh
@@ -12,7 +12,7 @@ _stage_run_tests() {
     project_location=$(get_project_location)
 
     local test_selection=""
-    local phpunit=${DRUPAL_TESTING_DRUPAL_INSTALLATION_DIRECTORY}/${composer_bin_dir}/phpunit
+    local phpunit=${DRUPAL_TESTING_DRUPAL_INSTALLATION_DIRECTORY}/${composer_bin_dir}/paratest
 
     if [[ ${DRUPAL_TESTING_TEST_GROUP} ]]; then
        test_selection="${test_selection} --group ${DRUPAL_TESTING_TEST_GROUP}"
@@ -26,7 +26,7 @@ _stage_run_tests() {
        test_selection="${test_selection} --filter ${DRUPAL_TESTING_TEST_FILTER}"
     fi
 
-    local runtest="php ${phpunit} --verbose --debug --configuration ${docroot}/core ${test_selection} ${project_location}"
+    local runtest="php ${phpunit} -p4 --verbose --debug --configuration ${docroot}/core ${test_selection} ${project_location}"
 
     eval "${runtest}" || exit 1
 }
