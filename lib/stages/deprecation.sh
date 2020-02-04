efc730326cced1c154c40dd7cce81a378d09dc0f
Bosen Daniel <daniel.bosen@burda.com>
2020-02-04 17:33:08 +0100

    get composer bin dir from composer file

--- lib/stages/deprecation.sh
+++ lib/stages/deprecation.sh
@@ -4,11 +4,12 @@ _stage_deprecation() {
     printf "Checking for deprecations.\n\n"
 
     local project_location=$(get_project_location)
+    local composer_bin_dir=$(get_composer_bin_directory)
 
     if ${DRUPAL_TESTING_TEST_DEPRECATION}; then
         cp phpstan.neon ${DRUPAL_TESTING_DRUPAL_INSTALLATION_DIRECTORY}
         cd ${DRUPAL_TESTING_DRUPAL_INSTALLATION_DIRECTORY}
-        vendor/bin/phpstan analyse --memory-limit 300M ${project_location}
+        "${composer_bin_dir}"/bin/phpstan analyse --memory-limit 300M ${project_location}
         cd -
     fi
 }
