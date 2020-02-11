dffeff38a5c0ff8ecabc168cd9732414548bc17a
Christian Fritsch <chr.fritsch@gmx.net>
2020-02-11 10:24:02 +0100

    fix: install correct dev version

--- lib/stages/prepare_build.sh
+++ lib/stages/prepare_build.sh
@@ -15,7 +15,8 @@ _stage_prepare_build() {
     # Build is based on drupal project
     if [[ ${DRUPAL_TESTING_COMPOSER_PROJECT} == "drupal/recommended-project" ]]; then
         composer create-project "${DRUPAL_TESTING_COMPOSER_PROJECT}:${DRUPAL_TESTING_DRUPAL_VERSION}" "${DRUPAL_TESTING_DRUPAL_INSTALLATION_DIRECTORY}" --no-interaction --no-install
-        composer require drupal/core-dev --dev --no-update --working-dir="${DRUPAL_TESTING_DRUPAL_INSTALLATION_DIRECTORY}"
+        installed_version=$(composer show 'drupal/core' | grep 'versions' | grep -o -E '[^ ]+$')
+        composer require drupal/core-dev:"${installed_version}" --dev --no-update --working-dir="${DRUPAL_TESTING_DRUPAL_INSTALLATION_DIRECTORY}"
     else
         composer create-project "${DRUPAL_TESTING_COMPOSER_PROJECT}" "${DRUPAL_TESTING_DRUPAL_INSTALLATION_DIRECTORY}" --no-interaction --no-install
         # Require the specific Drupal core version we need, as well as the corresponding dev-requirements
