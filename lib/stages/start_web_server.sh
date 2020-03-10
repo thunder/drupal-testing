9a47f63706a9fbff4f53b18a9c7c21b265fcd23d
Bosen Daniel <daniel.bosen@burda.com>
2020-03-10 15:27:36 +0100

    remove unused variable

--- lib/stages/start_web_server.sh
+++ lib/stages/start_web_server.sh
@@ -3,13 +3,8 @@
 _stage_start_web_server() {
     printf "Starting web server\n\n"
 
-    local composer_bin_dir
     local docroot
-
     docroot=$(get_distribution_docroot)
-    composer_bin_dir=$(get_composer_bin_directory)
-
-    local drush="${DRUPAL_TESTING_DRUPAL_INSTALLATION_DIRECTORY}/${composer_bin_dir}/drush  --root=${docroot}"
 
     if ! port_is_open "${DRUPAL_TESTING_HTTP_HOST}" "${DRUPAL_TESTING_HTTP_PORT}"; then
         php -S "${DRUPAL_TESTING_HTTP_HOST}":"${DRUPAL_TESTING_HTTP_PORT}" -t "${docroot}" >/dev/null 2>&1 &
