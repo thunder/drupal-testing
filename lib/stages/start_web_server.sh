bd3f01de7990217d1226e69d0505363c74dbcb23
Christian Fritsch <chr.fritsch@gmx.net>
2020-03-11 11:18:41 +0100

    improvement: use plain php webserver

--- lib/stages/start_web_server.sh
+++ lib/stages/start_web_server.sh
@@ -3,22 +3,11 @@
 _stage_start_web_server() {
     printf "Starting web server\n\n"
 
-    local composer_bin_dir
     local docroot
-
     docroot=$(get_distribution_docroot)
-    composer_bin_dir=$(get_composer_bin_directory)
-
-    local drush="${DRUPAL_TESTING_DRUPAL_INSTALLATION_DIRECTORY}/${composer_bin_dir}/drush  --root=${docroot}"
 
     if ! port_is_open "${DRUPAL_TESTING_HTTP_HOST}" "${DRUPAL_TESTING_HTTP_PORT}"; then
-        local runserver_flags=""
-
-        if ! ${DRUPAL_TESTING_VERBOSE}; then
-            runserver_flags="--quiet"
-        fi
-
-        ${drush} ${runserver_flags} runserver "http://${DRUPAL_TESTING_HTTP_HOST}:${DRUPAL_TESTING_HTTP_PORT}" >/dev/null 2>&1 &
-        wait_for_port "${DRUPAL_TESTING_HTTP_HOST}" "${DRUPAL_TESTING_HTTP_PORT}"
+        php -S "${DRUPAL_TESTING_HTTP_HOST}":"${DRUPAL_TESTING_HTTP_PORT}" -t "${docroot}" >/dev/null 2>&1 &
+        wait_for_port "${DRUPAL_TESTING_HTTP_HOST}" "${DRUPAL_TESTING_HTTP_PORT}" 30
     fi
 }
