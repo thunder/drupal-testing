1baec2a4f8c952cc0b92b6e3ce1e16146d98d318
Bosen Daniel <daniel.bosen@burda.com>
2020-04-02 11:58:47 +0200

    default theme

--- tests/module/tests/FunctionalJavascript/ModuleControllerTest.php
+++ tests/module/tests/FunctionalJavascript/ModuleControllerTest.php
@@ -21,6 +21,11 @@ class ModuleControllerTest extends WebDriverTestBase {
   /**
    * {@inheritdoc}
    */
+  protected $defaultTheme = 'stable';
+
+  /**
+   * {@inheritdoc}
+   */
   public function setUp() {
     parent::setUp();
 
