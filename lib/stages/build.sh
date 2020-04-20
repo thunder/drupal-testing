71ea2c6244f2336edcf3d41b5e7a2d1c7dbd9eb5
Daniel Bosen <daniel.bosen@burda.com>
2020-04-20 15:43:37 +0200

    Rename test project during the test.

--- lib/stages/build.sh
+++ lib/stages/build.sh
@@ -20,6 +20,9 @@ _stage_build() {
     cd "${DRUPAL_TESTING_DRUPAL_INSTALLATION_DIRECTORY}" || exit
     composer update ${composer_arguments}
 
+    # We can cleanup the name change now.
+    composer config name "${DRUPAL_TESTING_COMPOSER_NAME}" --working-dir="${DRUPAL_TESTING_PROJECT_BASEDIR}"
+
     installed_version=$(composer show 'drupal/core' | grep 'versions' | grep -o -E '[^ ]+$')
     major_version="$(cut -d'.' -f1 <<<"${installed_version}")"
     minor_version="$(cut -d'.' -f2 <<<"${installed_version}")"
