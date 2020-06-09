bae8982245735cdd4028774b90b33514a7b3d74f
Christian Fritsch <chr.fritsch@gmx.net>
2020-06-09 11:14:05 +0200

    Apply core patch #3143604 as a temporary solution

--- lib/stages/build.sh
+++ lib/stages/build.sh
@@ -30,6 +30,14 @@ _stage_build() {
     # Back to previous directory.
     cd - || exit
 
+    if [[ ${major_version} -gt 8 ]]; then
+        # Apply core patch
+        cd "${docroot}" || exit
+        wget https://www.drupal.org/files/issues/2020-06-08/3143604-8_0.patch
+        patch -p1 < 3143604-8_0.patch
+        cd - || exit
+    fi
+
     # Copy default settings and append config sync directory.
     local sites_directory="${docroot}/sites/default"
     cp "${sites_directory}/default.settings.php" "${sites_directory}/settings.php"
