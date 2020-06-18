fb5d0e40e1a84f1db55894fd5cd9c687a442e72e
Christian Fritsch <chr.fritsch@gmx.net>
2020-06-18 10:38:15 +0200

    fix: using dump

--- lib/stages/install.sh
+++ lib/stages/install.sh
@@ -30,10 +30,15 @@ _stage_install() {
         echo "\$config_directories = [ CONFIG_SYNC_DIRECTORY => '${DRUPAL_TESTING_CONFIG_SYNC_DIRECTORY}' ];" >>"${sites_directory}/settings.php"
     fi
 
-
     if ${DRUPAL_TESTING_INSTALL_FROM_CONFIG} = true; then
         ${drush} --verbose --db-url="${SIMPLETEST_DB}" --yes --existing-config site-install
     else
         ${drush} --verbose --db-url="${SIMPLETEST_DB}" --yes site-install "${DRUPAL_TESTING_TEST_PROFILE}" "${DRUPAL_TESTING_INSTALLATION_FORM_VALUES}"
+    fi
+
+    if [[ ${DRUPAL_TESTING_TEST_DUMP_FILE} != "" ]]; then
+        cd "${docroot}" || exit
+        php core/scripts/db-tools.php dump-database-d8-mysql >"${docroot}/${DRUPAL_TESTING_TEST_DUMP_FILE}"
+        cd - || exit
     fi
 }
