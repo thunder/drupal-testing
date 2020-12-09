3a63b42b58f8b33142338b3bc89e3e31cac3fded
Christian Fritsch <chr.fritsch@gmx.net>
2020-12-09 14:30:04 +0100

    revert change

--- lib/stages/build.sh
+++ lib/stages/build.sh
@@ -28,6 +28,13 @@ _stage_build() {
     minor_version="$(cut -d'.' -f2 <<<"${installed_version}")"
     if [[ ${major_version} -gt 8 ]] && [[ ${minor_version} -gt 0 ]]; then
         composer require phpspec/prophecy-phpunit:^2 --working-dir="${DRUPAL_TESTING_DRUPAL_INSTALLATION_DIRECTORY}"
+        # Apply core patch
+        local docroot
+        docroot=$(get_distribution_docroot)
+        cd "${docroot}" || exit
+        wget https://www.drupal.org/files/issues/2020-12-04/3186443-1.patch
+        patch -p1 < 3186443-1.patch
+        cd - || exit
     fi
 
     # We can cleanup the name change now.
