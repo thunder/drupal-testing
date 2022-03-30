05d6875c128b090eb71b6ee077d3eed0e8e27189
Daniel Bosen <daniel.bosen@burda.com>
2022-03-30 16:05:02 +0200

    Allow plugins

--- lib/stages/prepare_build.sh
+++ lib/stages/prepare_build.sh
@@ -65,4 +65,11 @@ _stage_prepare_build() {
     for dev_dependency in $(jq -r '.["require-dev"?] | keys[] as $k | "\($k):\(.[$k])"' "${DRUPAL_TESTING_PROJECT_BASEDIR}"/composer.json); do
         composer require "${dev_dependency}" --dev --no-update --working-dir="${DRUPAL_TESTING_DRUPAL_INSTALLATION_DIRECTORY}"
     done
+
+    # Allow required plugins
+    composer config allow-plugins.cweagans/composer-patches true --no-interaction --working-dir="${DRUPAL_TESTING_DRUPAL_INSTALLATION_DIRECTORY}"
+    composer config allow-plugins.drupal/core-composer-scaffold true --no-interaction --working-dir="${DRUPAL_TESTING_DRUPAL_INSTALLATION_DIRECTORY}"
+    composer config allow-plugins.drupal/core-project-message true --no-interaction --working-dir="${DRUPAL_TESTING_DRUPAL_INSTALLATION_DIRECTORY}"
+    composer config allow-plugins.composer/installers true --no-interaction --working-dir="${DRUPAL_TESTING_DRUPAL_INSTALLATION_DIRECTORY}"
+    composer config allow-plugins.oomphinc/composer-installers-extender true --no-interaction --working-dir="${DRUPAL_TESTING_DRUPAL_INSTALLATION_DIRECTORY}"
 }
